220020a30bf49f8e31b002a059012a61b81c549d
Sury108 <suryabehr@protonmail.com>
2020-03-20 11:21:31 +0100

    CameraMovement

---
+++ Assets/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+
+    // Update is called once per frame
+    void Update(){
+
+        Vector3 pos = transform.position;
+
+        if  (Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if  (Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if  (Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if  (Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+        
+    }
+}
