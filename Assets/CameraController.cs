0af9cf7bd6e9c0983930122143ea55b1798fb950
Sury108 <suryabehr@protonmail.com>
2020-03-24 11:48:54 +0100

    Update CameraController.cs

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour {
     public float scrollSpeed = 10f;
     public float minY = 10f;
     public float maxY = 200f;
+
     // Update is called once per frame
     void Update(){
 
